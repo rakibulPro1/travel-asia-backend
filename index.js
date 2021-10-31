e0adafac165a64d49944cd78c264f2116aa71db4
Rakibul Islam <rakibul102760@gmail.com>
2021-10-31 14:23:22 +0600

    updated-server-with-heroku

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const port = process.env.PORT || 5000;
 require("dotenv").config();
+const ObjectId = require("mongodb").ObjectId;
 const { MongoClient } = require("mongodb");
 
 const app = express();
@@ -20,7 +21,43 @@ async function run() {
   try {
     await client.connect();
     const database = client.db("travel_asia");
-    const services = database.collection("services");
+    const toursesCollection = database.collection("tourses");
+    const bookingCollection = database.collection("bookings");
+
+    // GET api to get all tourses
+    app.get("/tourses", async (req, res) => {
+      const cursor = toursesCollection.find({});
+      const tourses = await cursor.toArray();
+      res.send(tourses);
+    });
+
+    // GET API to get single tours
+    app.get("/tourses/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await toursesCollection.findOne(query);
+      res.send(result);
+    });
+    // POST API tourses
+    app.post("/tourses", async (req, res) => {
+      const tours = req.body;
+      const result = await toursesCollection.insertOne(tours);
+      res.json(result);
+    });
+
+    // POST API to Booking post
+    app.post("/bookings", async (req, res) => {
+      const booking = req.body;
+      const result = await bookingCollection.insertOne(booking);
+      res.json(result);
+    });
+
+    // GET API to all bookings
+    app.get("/bookings", async (req, res) => {
+      const cursor = bookingCollection.find({});
+      const bookings = await cursor.toArray();
+      res.send(bookings);
+    });
 
     console.log("database connected");
   } finally {
@@ -30,17 +67,9 @@ async function run() {
 run().catch(console.dir);
 
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.send("hellow world");
 });
-app.get("/users", (req, res) => {
-  res.send("This is users page");
-});
-app.get("/services", (req, res) => {
-  res.send("This is service page");
-});
-app.get("/comments", (req, res) => {
-  res.send("This is service page");
-});
+
 app.listen(port, () => {
   console.log(`Server listening at http://localhost:${port}`);
 });
